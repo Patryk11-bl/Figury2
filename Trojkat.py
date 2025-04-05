6d246c9f78f237bebab42df4469aee3c449d6b43
Patryk11-bl <bloch.patryk.pb1@gmail.com>
2025-04-05 22:24:35 +0200

    dodanie trojkata

---
+++ Trojkat.py
@@ -0,0 +1,26 @@
+class Trojkat:
+    bok1 = 0
+    bok2 = 0
+    bok3 = 0
+    wysokosc = 0
+    Pole_Troj = 0
+    Obwod_Troj =  0
+    
+    def __init__(self,bok1,bok2,bok3, wysokosc):
+        self.bok1 = bok1
+        self.bok2 = bok2
+        self.bok3 = bok3
+        self.wysokosc = wysokosc
+        self.Pole_Troj = bok1*wysokosc/2
+        self.Obwod_Troj = bok1+bok2+bok3
+        
+    def wysPole(self):
+        print(f'Pole trojkata to {self.Pole_Troj}')
+
+    def wysObwod(self):
+        print(f'Obwod trojkata to {self.Obwod_Troj}')
+
+trojkat1 = Trojkat(6,6,6,6)
+
+trojkat1.wysObwod()
+trojkat1.wysPole()
